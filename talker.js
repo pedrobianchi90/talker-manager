e65fb8b14afdfa17b0aaa12ce292036c5dc06bae
Pedro Bianchi <pedroedm@gmail.com>
2022-06-21 08:58:43 -0300

    Cria o endpoint GET /talker/:id

--- talker.js
+++ talker.js
@@ -4,11 +4,23 @@ const fs = require('fs');
 
 const router = Router();
 
-router.get('/', (_req, res) => {
-    const file = fs.readFileSync('./talker.json', 'utf8');
-    if (!file) return res.status(200).json([]);
-    const parsed = JSON.parse(file);
-    res.status(200).json(parsed);
+const readTalker = (req, res, next) => {
+  const file = fs.readFileSync('./talker.json', 'utf8');
+  if (!file) return res.status(200).json([]);
+  const parse = JSON.parse(file);
+  req.talkers = parse;
+  next();
+};
+router.get('/', readTalker, (req, res) => {
+  const { talkers } = req;
+  res.status(200).json(talkers);
 });
 
+router.get('/:id', readTalker, (req, res) => {
+  const { talkers } = req;
+  const { id } = req.params;
+  const currentTalker = talkers.find((talker) => talker.id === Number(id));
+  if (!currentTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  res.status(200).json(currentTalker);
+});
 module.exports = router;
